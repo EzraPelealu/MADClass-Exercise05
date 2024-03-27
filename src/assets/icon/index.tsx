22c967620a4a7b722ae91f27d5176159879bbfaa
Ezra Pelealu <116333571+EzraPelealu@users.noreply.github.com>
2024-03-28 00:11:30 +0800

    Delete src directory

--- src/assets/icon/index.tsx
+++
@@ -1,6 +0,0 @@
-import Logo from './logo.svg';
-import BackButton from './BackButton.svg';
-import Profile from './profile.jpg';
-import Subtract from './Subtract.svg';
-
-export {Logo, BackButton, Profile, Subtract};
